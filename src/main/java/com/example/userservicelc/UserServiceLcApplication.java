335fad5dd67da8c4ccd6161ab082c837f1e59930
theali <thealiergashev@gmail.com>
2023-11-01 20:34:12 +0500

    Initial commit

---
+++ src/main/java/com/example/userservicelc/UserServiceLcApplication.java
@@ -0,0 +1,13 @@
+package com.example.userservicelc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceLcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userservicelc/UserServiceLcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservicelc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceLcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
